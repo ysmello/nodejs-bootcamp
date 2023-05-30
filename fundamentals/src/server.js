fb6ac7c1cf7587604bb5ce8ad95da418a1b13f30
Yuri Mello <yuridemeello@gmail.com>
2023-05-30 20:57:30 -0300

    creating middleware

--- fundamentals/src/server.js
+++ fundamentals/src/server.js
@@ -1,21 +1,12 @@
 import http from 'http'
+import { json } from './middlewares/json.js'
 
 const users = []
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
 
-  const buffers = []
-
-  for await (const chunk of req) {
-    buffers.push(chunk)
-  }
-
-  try {
-    req.body = JSON.parse(Buffer.concat(buffers).toString())
-  } catch {
-    req.body = null
-  }
+  await json(req, res)
 
   if (method === 'GET' && url === '/users') {
     return res
