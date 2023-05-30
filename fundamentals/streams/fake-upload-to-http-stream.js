95ae3a4937654ef3775a3858f97c4c9b86209f38
Yuri Mello <yuridemeello@gmail.com>
2023-05-30 20:16:19 -0300

    consuming a complete stream

--- fundamentals/streams/fake-upload-to-http-stream.js
+++ fundamentals/streams/fake-upload-to-http-stream.js
@@ -7,7 +7,7 @@ class OneToHundredStream extends Readable {
     const i = this.index++
 
     setTimeout(() => {
-      if (i > 100) {
+      if (i > 5) {
         this.push(null)
       } else {
         const buf = Buffer.from(String(i));
@@ -20,5 +20,10 @@ class OneToHundredStream extends Readable {
 
 fetch('http://localhost:3334', {
   method: 'POST',
-  body: new OneToHundredStream()
+  body: new OneToHundredStream(),
+  
+}).then(response => {
+  return response.text()
+}).then(data => {
+  console.log(data)
 })
