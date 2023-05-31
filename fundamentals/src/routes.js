1b09cca2a29a5b6a34c36720f6183a071810cc2c
Yuri Mello <yuridemeello@gmail.com>
2023-05-30 22:35:45 -0300

    app routes

--- fundamentals/src/routes.js
+++ fundamentals/src/routes.js
@@ -1,21 +1,27 @@
 import { randomUUID } from "crypto"
 import { Database } from "./database.js"
+import { buildRoutePath } from "./utils/build-route-path.js"
 
 const database = new Database()
 
 export const routes = [
   {
     method: 'GET',
-    path: '/users',
+    path: buildRoutePath('/users'),
     handle: (req, res) => {
-      const users = Database.select('users')
+      const { search } = req.query
+
+      const users = database.select('users', search ? {
+        name: search,
+        email: search
+      } : null)
 
       return res.end(JSON.stringify(users))
     }
   },
   {
     method: 'POST',
-    path: '/users',
+    path: buildRoutePath('/users'),
     handle: (req, res) => {
       const { name, email } = req.body
 
@@ -28,6 +34,32 @@ export const routes = [
       database.insert('users', user)
 
       return res.writeHead(201).end()
+    }
+  },
+  {
+    method: 'PUT',
+    path: buildRoutePath('/users/:id'),
+    handle: (req, res) => {
+      const { id } = req.params
+      const { name, email } = req.body
+
+      database.update('users', id, {
+        name,
+        email
+      })
+
+      return res.writeHead(204).end()
+    }
+  },
+  {
+    method: 'DELETE',
+    path: buildRoutePath('/users/:id'),
+    handle: (req, res) => {
+      const { id } = req.params
+
+      database.delete('users', id)
+
+      return res.writeHead(204).end()
     }
   }
 ]
--- fundamentals/src/server.js
+++ fundamentals/src/server.js
@@ -1,6 +1,7 @@
-import http from 'http'
+import http from 'node:http'
 import { json } from './middlewares/json.js'
 import { routes } from './routes.js'
+import { extractQueryParams } from './utils/extract-query-params.js'
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
@@ -8,10 +9,17 @@ const server = http.createServer(async (req, res) => {
   await json(req, res)
 
   const route = routes.find(route => {
-    return route.method === method && route.path === url
+    return route.method === method && route.path.test(url)
   })
 
   if (route) {
+    const routeParams = req.url.match(route.path)
+
+    const { query, ...params } = routeParams.groups
+
+    req.params = params
+    req.query = query ? extractQueryParams(query) : {}
+
     return route.handle(req, res)
   }
 
