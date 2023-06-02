ee84e76b14756f172efb55c493aa253425568d00
Yuri Mello <yuridemeello@gmail.com>
2023-06-01 23:39:15 -0300

    initial project and database

---
+++ api-rest-nodejs/src/server.ts
@@ -0,0 +1,21 @@
+import fastify from 'fastify'
+import { knex } from './database'
+import { env } from './env'
+
+const app = fastify()
+
+app.get('/hello', async () => {
+  const transaction = await knex('transactions')
+    .where('amount', 1000)
+    .select('*')
+
+  return transaction
+})
+
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('HTTP Server Running!')
+  })
