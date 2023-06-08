28effc09c92e517619b944f6b53140cb85411284
Yuri Mello <yuridemeello@gmail.com>
2023-06-08 01:44:05 -0300

    use case authenticate and register

--- api-solid/src/repositories/in-memory-users-repository.ts
+++ api-solid/src/repositories/in-memory-users-repository.ts
@@ -1,11 +1,30 @@
-import { Prisma } from '@prisma/client'
+import { Prisma, User } from '@prisma/client'
+import { UsersRepository } from './users-repository'
 
-export class InMemoryUsersRepository {
-  public users: any[] = []
+export class InMemoryUsersRepository implements UsersRepository {
+  public users: User[] = []
 
-  async create(data: Prisma.UserCreateInput) {
-    this.users.push(data)
+  async findByEmail(email: string): Promise<User | null> {
+    const user = this.users.find((user) => user.email === email)
 
-    return data
+    if (!user) {
+      return null
+    }
+
+    return user
+  }
+
+  async create(data: Prisma.UserCreateInput): Promise<User> {
+    const user = {
+      id: 'user-1',
+      name: data.name,
+      email: data.email,
+      password_hash: data.password_hash,
+      created_at: new Date(),
+    }
+
+    this.users.push(user)
+
+    return user
   }
 }
